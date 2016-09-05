db6bfc30d2b28e588059999c1ed3af74d0b28b23
Wolf <wolfwolfhan@gmail.com>
2016-09-05 18:52:59 +0800

    Add the layout for new channel screen

--- app/src/main/java/org/attentiveness/news/channel/NewsChannelActivity.java
+++ app/src/main/java/org/attentiveness/news/channel/NewsChannelActivity.java
@@ -1,11 +1,7 @@
 package org.attentiveness.news.channel;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import org.attentiveness.news.R;
 
@@ -15,17 +11,6 @@ public class NewsChannelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_channel);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
