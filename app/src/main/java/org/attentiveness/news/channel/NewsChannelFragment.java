5e883976ac6be7a2de266f79d85eb16c608924cc
Wolf <wolfwolfhan@gmail.com>
2016-09-22 21:00:33 +0800

    Add the toggle button

--- app/src/main/java/org/attentiveness/news/channel/NewsChannelFragment.java
+++ app/src/main/java/org/attentiveness/news/channel/NewsChannelFragment.java
@@ -2,10 +2,13 @@ package org.attentiveness.news.channel;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import org.attentiveness.news.R;
 import org.attentiveness.news.base.BaseFragment;
@@ -21,6 +24,12 @@ public class NewsChannelFragment extends BaseFragment {
     GridLayout mGlMyChannelList;
     @Bind(R.id.gl_channel_list_recommended)
     GridLayout mGlChannelListRecommended;
+    @Bind(R.id.btn_edit_or_done)
+    Button mBtnEditOrDone;
+    @Bind(R.id.iv_remove)
+    ImageView mIvRemove;
+
+    private BUTTON_STATUS mStatus;
 
     public static NewsChannelFragment newInstance() {
         return new NewsChannelFragment();
@@ -37,9 +46,44 @@ public class NewsChannelFragment extends BaseFragment {
         return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        mStatus = BUTTON_STATUS.DONE;
+    }
+
     @OnClick(R.id.tv_close)
     void closeScreen() {
         getActivity().finish();
+    }
+
+    @OnClick(R.id.btn_edit_or_done)
+    void editOrDone() {
+        if (mBtnEditOrDone.getText().toString().equals(getString(R.string.btn_edit))) {
+            mBtnEditOrDone.setText(getString(R.string.btn_done));
+            mStatus = BUTTON_STATUS.EDITABLE;
+        } else {
+            mBtnEditOrDone.setText(getString(R.string.btn_edit));
+            mStatus = BUTTON_STATUS.DONE;
+        }
+        updateViews();
+    }
+
+    private void updateViews() {
+        if (mStatus == BUTTON_STATUS.EDITABLE) {
+            mIvRemove.setVisibility(View.VISIBLE);
+        } else {
+            mIvRemove.setVisibility(View.GONE);
+        }
+    }
+
+    public enum BUTTON_STATUS {
+        EDITABLE,
+        DONE
     }
 
 }
